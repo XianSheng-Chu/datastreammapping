cd2abfa36d58e79f8db521a4dd89846b9e8d1eb0
xiansheng-chu <3551143669@qq.com>
2026-02-03 22:07:01 +0800

    项目变更-20260203

--- src/datastreammapping/symbol_table/runtime_scopes.py
+++ src/datastreammapping/symbol_table/runtime_scopes.py
@@ -5,7 +5,8 @@ class SelectScope(QueryScope):
     def __init__(self, parent:QueryScope,query_name,ast_node,scope_type=ScopeType.QUERY):
         super().__init__(parent,query_name)
 
-        self.columns:list = []
+        self.output_columns:list = []
+        self.columns: list = []
         parent.add_child_scope(self)
         self.set_ast_node(ast_node)
         self.fetch_columns(ast_node)
@@ -16,4 +17,11 @@ class SelectScope(QueryScope):
         outputs = node.named_selects
         item = node.selects
         for i in  range(0,len(outputs)):
-            self.columns.append((outputs[i],item[i]))
+            self.output_columns.append((outputs[i],item[i]))
+            if outputs[i].key=="alias":
+                self.columns.append(item[i].this)
+            else:
+                self.columns.append(item[i])
+
+    def add_node(self,ast_node:Expression):
+        node = ast_node
